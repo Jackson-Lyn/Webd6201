ee7b215380eb91db3fd1317021fa4d2d0f71373c
Jackson-Lyn <jason.lyn1@dcmail.ca>
2023-03-05 15:22:20 -0500

    Created login page

--- ICE6_starter/scripts/fetch_script.js
+++ ICE6_starter/scripts/fetch_script.js
@@ -1,16 +1,32 @@
 console.log("fetch_script.js loaded");
 
 // get the button for fetch
+let btnFetch = $("#fetchJoke");
 
 // create a url variable
+let url_fetch = 'https://icanhazdadjoke.com/'
 
 //need a header Accept value set to 'application/json'
+let myFetchHeaders = {"Accept": "application/json"}
 
 // create the callback for the click
+$(btnFetch).click(() => {
     // use fetch
+    fetch(url_fetch, {
         // give the necessary header data
+        headers: myFetchHeaders
+    })
         // first then() to recieve the promise
-            // send the json from the promise on to the next then()
+        .then((response) => {
+             // send the json from the promise on to the next then()
+            return response.json();
+        }).then((jsonResponse) => {
             // console log the json
+            console.log(jsonResponse['joke']);
             // set the output
-
+            $('#output').text(jsonResponse['joke']);
+        })
+           
+            
+            
+        });
---
+++ Lab1/scripts/login.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    // Get references to the HTML elements
+    const $navbarText = $(".navbar-text");
+  
+    // Add event listener for form submission
+    $("form").submit((event) => {
+      event.preventDefault(); // Prevent default form submission behavior
+  
+      const username = $("#inputUsername").val();
+      const password = $("#inputPassword").val();
+  
+      // Validate username and password
+      if (username === "admin" && password === "password") {
+        // Insert username into the navbar
+        $navbarText.html(`Contact Us | ${username} | <a href="#">Logout</a>`);
+      } else {
+        // Show error message
+        alert("Invalid username or password.");
+      }
+    });
+  });
